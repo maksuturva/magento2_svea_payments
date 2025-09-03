a91853c993160bc6002d1d1cfaa25955bd500724
Robert Ahlskog <robert.ahlskog@solidcode.fi>
2025-09-03 19:19:33 +0300

    Clean up shiping row builder from unneccessary complexity

--- Gateway/Request/RowData/ShippingRowBuilder.php
+++ Gateway/Request/RowData/ShippingRowBuilder.php
@@ -2,14 +2,8 @@
 
 namespace Svea\SveaPayment\Gateway\Request\RowData;
 
-use Magento\Checkout\Model\Session;
-use Magento\Customer\Model\ResourceModel\GroupRepository;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
-use Magento\Tax\Helper\Data as TaxHelper;
-use Magento\Tax\Model\Calculation;
 use Svea\SveaPayment\Gateway\Data\AmountHandler;
-use Svea\SveaPayment\Gateway\Data\Order\OrderAdapterFactory;
 use Svea\SveaPayment\Gateway\Request\RowBuilderInterface;
 use Svea\SveaPayment\Gateway\SubjectReaderInterface;
 use function __;
@@ -20,36 +14,6 @@ class ShippingRowBuilder implements RowBuilderInterface
      * @var SubjectReaderInterface
      */
     private $subjectReader;
-
-    /**
-     * @var TaxHelper
-     */
-    private $taxHelper;
-
-    /**
-     * @var Calculation
-     */
-    private $calculationModel;
-
-    /**
-     * @var Session
-     */
-    private $checkoutSession;
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    /**
-     * @var GroupRepository
-     */
-    private $groupRepository;
-
-    /**
-     * @var OrderAdapterFactory
-     */
-    private $orderAdapterFactory;
 
     /**
      * @var TimezoneInterface
@@ -63,22 +27,10 @@ class ShippingRowBuilder implements RowBuilderInterface
 
     public function __construct(
         SubjectReaderInterface $subjectReader,
-        TaxHelper $taxHelper,
-        Calculation $calculationModel,
-        Session $checkoutSession,
-        ScopeConfigInterface $scopeConfig,
-        GroupRepository $groupRepository,
-        OrderAdapterFactory $orderAdapterFactory,
         TimezoneInterface $timezone,
         AmountHandler $amountHandler
     ) {
         $this->subjectReader  = $subjectReader;
-        $this->taxHelper = $taxHelper;
-        $this->calculationModel = $calculationModel;
-        $this->checkoutSession = $checkoutSession;
-        $this->scopeConfig = $scopeConfig;
-        $this->groupRepository = $groupRepository;
-        $this->orderAdapterFactory = $orderAdapterFactory;
         $this->timezone = $timezone;
         $this->amountHandler = $amountHandler;
     }
@@ -92,56 +44,26 @@ class ShippingRowBuilder implements RowBuilderInterface
         $orderAdapter = $paymentDO->getOrder();
 
         $shippingDescription = $orderAdapter->getShippingDescription() ?: 'Free Shipping';
-        $shippingCost = $orderAdapter->getBaseShippingAmount() ?? 0.0;
-        $taxId = $this->taxHelper->getShippingTaxClass($orderAdapter->getStoreId());
-        $request = $this->calculationModel->getRateRequest();
-
-        $request->setCustomerClassId($this->getCustomerTaxClass())->setProductClassId($taxId);
-
-        $shippingTax = $orderAdapter->getBaseShippingTaxAmount() ?? 0;
-        $shippingTaxRate = $this->getShippingTaxRate($shippingTax, $shippingCost);
+        $shippingInclTax = $orderAdapter->getBaseShippingAmountInclTax();
+        $shippingTaxRate = $orderAdapter->getShippingTaxRate();
 
         $row = [
             self::NAME => __('Shipping'),
             self::DESC => $shippingDescription,
             self::QUANTITY => 1,
             self::DELIVERY_DATE => $this->timezone->date()->format('d.m.Y'),
-            self::PRICE_GROSS => $this->amountHandler->formatFloat($shippingCost + $shippingTax),
+            self::PRICE_GROSS => $this->amountHandler->formatFloat($shippingInclTax),
             self::VAT => $this->amountHandler->formatFloat($shippingTaxRate),
             self::DISCOUNT_PERCENTAGE => '0,00',
             self::TYPE => 2,
         ];
 
-        $sellerCosts += $shippingCost + $shippingTax;
+        $sellerCosts += $shippingInclTax;
 
         return [
             self::TOTAL_AMOUNT => $totalAmount,
             self::SELLER_COSTS => $sellerCosts,
             self::ROW => $row,
         ];
-    }
-
-    private function getCustomerTaxClass(): int
-    {
-        $customerGroup = $this->checkoutSession->getQuote()->getCustomerGroupId();
-        if (!$customerGroup) {
-            $customerGroup = $this->scopeConfig->getValue('customer/create_account/default_group');
-        }
-
-        return $this->groupRepository->getById($customerGroup)->getTaxClassId();
-    }
-
-    /**
-     * @param float $shippingTax
-     * @param float $shippingCost
-     * @return float|int
-     */
-    private function getShippingTaxRate(float $shippingTax, float $shippingCost)
-    {
-        if ($shippingCost === 0.0) {
-            return 0;
-        }
-
-        return (float)(\round(($shippingTax / $shippingCost) * 100 * 2) / 2);
     }
 }
